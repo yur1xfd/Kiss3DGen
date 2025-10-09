7e94bae31ddb3c1264159716098f6aed773d5dc6
yur1xfd <melnik.um@yandex.ru>
2025-10-09 14:20:20 +0300

    correct cuda validation

--- pipeline/kiss3d_wrapper_3d_to_3d.py
+++ pipeline/kiss3d_wrapper_3d_to_3d.py
@@ -793,7 +793,7 @@ def run_3d_to_3d(k3d_wrapper, input_mesh_path, prompt=None, use_controlnet=True,
     
     torch.cuda.empty_cache()
     
-    logger.warning(f"GPU memory allocated after load flux model on {flux_device}: {torch.cuda.memory_allocated(device=flux_device) / 1024**3} GB")
+    logger.warning(f"GPU memory allocated : {torch.cuda.memory_allocated() / 1024**3} GB")
     # recon from 3D Bundle image
     recon_mesh_path = k3d_wrapper.reconstruct_3d_bundle_image(gen_3d_bundle_image, save_intermediate_results=False,
                                                               isomer_radius=4.15, reconstruction_stage2_steps=50)
