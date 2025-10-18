ea9d2c1fa236d09d58c0b3410d784cacc493f63a
yur1xfd <melnik.um@yandex.ru>
2025-10-18 23:32:24 +0300

    add a3d meshes and editing prompts

--- pipeline/example_3d_to_3d.py
+++ pipeline/example_3d_to_3d.py
@@ -19,18 +19,21 @@ if __name__ == "__main__":
     refine_prompt = True
 
     #prompts = open('./examples/enhancement/prompts.txt', 'r').readlines()
-    prompts = open('./examples/enhancement/prompts_edit.txt', 'r').readlines()
+    prompts = open('./examples/enhancement/prompts_a3d_edit.txt', 'r').readlines()
 
     prompts_dict = {prompt.split(';')[0]: prompt.split(';')[1].strip() for prompt in prompts}
     
-    mesh_folder = './examples/enhancement/local_quality_3d_meshes'
+    mesh_folder = './examples/enhancement/a3d_meshes'
     for mesh_name in os.listdir(mesh_folder):
-        print("Now processing:", mesh_name)
-        print("Prompt:", prompts_dict[mesh_name])
-        end = time.time()
-        gen_save_path, recon_mesh_path = run_3d_to_3d(k3d_wrapper, os.path.join(mesh_folder, mesh_name, f'{mesh_name}.obj'), prompts_dict[mesh_name], use_controlnet, refine_prompt)
-        print(f"3d to 3d generation time: {time.time() - end} s")
-        # run_3d_to_3d(k3d_wrapper, os.path.join(mesh_folder, mesh_name, f'{mesh_name}.obj'), enable_redux, use_mv_rgb, use_controlnet)
+        if mesh_name in prompts_dict:
+            print("Now processing:", mesh_name)
+            print("Prompt:", prompts_dict[mesh_name])
+            end = time.time()
+            gen_save_path, recon_mesh_path = run_3d_to_3d(k3d_wrapper, os.path.join(mesh_folder, mesh_name, f'{mesh_name}.obj'), prompts_dict[mesh_name], use_controlnet, refine_prompt)
+            print(f"3d to 3d generation time: {time.time() - end} s")
+            # run_3d_to_3d(k3d_wrapper, os.path.join(mesh_folder, mesh_name, f'{mesh_name}.obj'), enable_redux, use_mv_rgb, use_controlnet)
 
-        os.system(f'cp -f {gen_save_path} {OUT_DIR}/{save_dir}/{mesh_name}_3d_bundle.png')
-        os.system(f'cp -f {recon_mesh_path} {OUT_DIR}/{save_dir}/{mesh_name}.glb')
+            os.system(f'cp -f {gen_save_path} {OUT_DIR}/{save_dir}/{mesh_name}_3d_bundle.png')
+            os.system(f'cp -f {recon_mesh_path} {OUT_DIR}/{save_dir}/{mesh_name}.glb')
+        else:
+            print('There is no editing instruction for this mesh')
